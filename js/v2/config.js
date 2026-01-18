13fbdb47a248c3d3adbe7a5af98ba4afcd9ef70d
pfsolidaireperpignan <pfsolidaireperpignan@gmail.com>
2026-01-18 20:34:04 +0100

    Update config.js

--- js/v2/config.js
+++ js/v2/config.js
@@ -3,31 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, getDoc, query, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// --- REMPLACEZ CECI PAR VOS CLES (Celles de l'ancien fichier) ---
-const firebaseConfig = {
-    apiKey: "AIzaSyDmsIkTjW2IFkIks5BUAnxLLnc7pnj2e0w",
-    authDomain: "pf-solidaire.firebaseapp.com",
-    projectId: "pf-solidaire",
-    storageBucket: "pf-solidaire.firebasestorage.app",
-    messagingSenderId: "485465343242",
-    appId: "1:485465343242:web:46d2a49f851a95907b26f3",
-    measurementId: "G-TWLLXKF0K4"
-};
-// -----------------------------------------------------------------
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-// Nouvelle table propre
-const COLLECTION_NAME = "factures_v2"; 
-
-export { db, auth, onAuthStateChanged, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, getDoc, query, orderBy, COLLECTION_NAME };/* Fichier : js/v2/config.js */
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, getDoc, query, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-// --- REMETTEZ VOS CLES ICI (Copiez depuis votre ancien fichier js/config.js) ---
+// --- REMPLACEZ PAR VOS VRAIES CLÉS API ---
 const firebaseConfig = {
     apiKey: "AIzaSyDmsIkTjW2IFkIks5BUAnxLLnc7pnj2e0w",
     authDomain: "pf-solidaire.firebaseapp.com",
@@ -43,7 +19,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// Nouvelle table propre (V2)
+// Nom de la nouvelle base de données propre (V2)
 const COLLECTION_NAME = "factures_v2"; 
 
 export { db, auth, onAuthStateChanged, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, getDoc, query, orderBy, COLLECTION_NAME };
