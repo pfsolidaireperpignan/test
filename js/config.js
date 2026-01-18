27b50aefd50145540a9448d74beb6fd9c25a9553
pfsolidaireperpignan <pfsolidaireperpignan@gmail.com>
2026-01-19 00:00:36 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,9 @@
-
 /* Fichier : js/config.js (RACINE) */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// --- REMETTEZ VOS CLÉS ICI ---
+// --- VOS CLES ---
 const firebaseConfig = {
     apiKey: "AIzaSyDmsIkTjW2IFkIks5BUAnxLLnc7pnj2e0w",
     authDomain: "pf-solidaire.firebaseapp.com",
@@ -13,11 +12,12 @@ const firebaseConfig = {
     messagingSenderId: "485465343242",
     appId: "1:485465343242:web:46d2a49f851a95907b26f3",
     measurementId: "G-TWLLXKF0K4"
+
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// Export indispensable pour que script.js fonctionne
+// CETTE LIGNE EST CELLE QUI MANQUE OU QUI EST INCOMPLETE :
 export { app, db, auth, collection, addDoc, getDocs, query, orderBy, limit, onAuthStateChanged, signInWithEmailAndPassword, signOut };
